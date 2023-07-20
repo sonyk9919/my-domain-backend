42a819dad2589cad6a2354e75ff01adf7691fd74
sonyk9919 <sonyk9919@gmail.com>
2023-07-20 10:23:04 +0900

    chore: nestjs initial build

---
+++ README.md
@@ -0,0 +1,1 @@
+# My Domain Backend
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+declare const module: any;
+
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+}
+bootstrap();
